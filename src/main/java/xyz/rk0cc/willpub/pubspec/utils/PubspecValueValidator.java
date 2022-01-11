7852b42983c2a0a5b029eb7447f3381c93395ee3
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-11 21:25:02 +0800

    Add assertion and validation

--- src/main/java/xyz/rk0cc/willpub/pubspec/utils/PubspecValueValidator.java
+++ src/main/java/xyz/rk0cc/willpub/pubspec/utils/PubspecValueValidator.java
@@ -1,5 +1,11 @@
 package xyz.rk0cc.willpub.pubspec.utils;
 
+import xyz.rk0cc.willpub.exceptions.pubspec.IllegalPubPackageNamingException;
+
+import javax.annotation.Nonnull;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 public final class PubspecValueValidator {
     /**
      * You can not create {@link PubspecValueValidator} and always throw {@link UnsupportedOperationException}.
@@ -8,5 +14,61 @@ public final class PubspecValueValidator {
         throw new UnsupportedOperationException("Validator contains static method only");
     }
 
+    public static boolean packageNaming(@Nonnull String packageName) {
+        return Pattern.matches("^[a-z0-9_]+$", packageName)
+                && Stream.of( // Reserve keyword from https://dart.dev/guides/language/language-tour#keywords
+                        "assert",
+                        "break",
+                        "case",
+                        "catch",
+                        "class",
+                        "const",
+                        "continue",
+                        "default",
+                        "do",
+                        "else",
+                        "enum",
+                        "extends",
+                        "false",
+                        "final",
+                        "finally",
+                        "for",
+                        "if",
+                        "in",
+                        "is",
+                        "new",
+                        "null",
+                        "rethrow",
+                        "return",
+                        "super",
+                        "switch",
+                        "this",
+                        "throw",
+                        "true",
+                        "try",
+                        "var",
+                        "void",
+                        "while",
+                        "with"
+                )
+                .noneMatch(packageName::equals);
+    }
 
+    public static final class ValueAssertion {
+        private ValueAssertion() {
+            throw new UnsupportedOperationException("No constructor for assertion");
+        }
+
+        public static void assertPackageNaming(@Nonnull String packageName) throws IllegalPubPackageNamingException {
+            if (!packageNaming(packageName)) throw new IllegalPubPackageNamingException(packageName);
+        }
+
+        public static void assertPackageNaming(
+                @Nonnull String packageName,
+                @Nonnull String assertFailedMessage
+        ) throws IllegalPubPackageNamingException {
+            if (!packageNaming(packageName))
+                throw new IllegalPubPackageNamingException(packageName, assertFailedMessage);
+        }
+    }
 }
