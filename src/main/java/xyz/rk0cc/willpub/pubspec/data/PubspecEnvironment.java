5aeb6bac0fed8dbb6eca1ff2b743fdafe2841f71
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-15 12:22:42 +0800

    Doc env

--- src/main/java/xyz/rk0cc/willpub/pubspec/data/PubspecEnvironment.java
+++ src/main/java/xyz/rk0cc/willpub/pubspec/data/PubspecEnvironment.java
@@ -7,9 +7,28 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.Serializable;
 
+/**
+ * Giving a requirement preference which indicating required SDK versions constraint is supported.
+ * <br/>
+ * Although it has "setter" methods implemented, it's only creating new {@link PubspecEnvironment} with given data
+ * to be modified only, just like {@link String}.
+ *
+ * @since 1.0.0
+ *
+ * @see <a href="https://dart.dev/tools/pub/pubspec#sdk-constraints">SDK constraint in Dart documentation</a>
+ */
 public final class PubspecEnvironment implements Serializable {
     private final PubSemVerConstraint sdk, flutter;
 
+    /**
+     * Create new environment preference of pubspec's environment.
+     *
+     * @param sdk Dart SDK constraint.
+     * @param flutter Flutter constraint, <code>null</code> if not applied.
+     *
+     * @throws IllegalVersionConstraintException Applying <code>sdk</code> with {@link PubConstraintPattern#CARET} or
+     *                                           missing {@link PubSemVerConstraint#start() lower constraint}.
+     */
     public PubspecEnvironment(@Nonnull PubSemVerConstraint sdk, @Nullable PubSemVerConstraint flutter)
             throws IllegalVersionConstraintException {
         if (sdk.constraintPattern() == PubConstraintPattern.CARET)
@@ -21,25 +40,60 @@ public final class PubspecEnvironment implements Serializable {
         this.flutter = flutter;
     }
 
+    /**
+     * Create new environment preference of pubspec's environment without Flutter constraint.
+     *
+     * @param sdk Dart SDK constraint.
+     *
+     * @throws IllegalVersionConstraintException Applying <code>sdk</code> with {@link PubConstraintPattern#CARET} or
+     *                                           missing {@link PubSemVerConstraint#start() lower constraint}.
+     */
     public PubspecEnvironment(@Nonnull PubSemVerConstraint sdk) throws IllegalVersionConstraintException {
         this(sdk, null);
     }
 
+    /**
+     * Get current SDK constraint.
+     *
+     * @return A SDK version constraint data.
+     */
     @Nonnull
     public PubSemVerConstraint sdk() {
         return sdk;
     }
 
+    /**
+     * Get current Flutter constraint.
+     *
+     * @return A Flutter version constraint data, <code>null</code> if not applied.
+     */
     @Nullable
     public PubSemVerConstraint flutter() {
         return flutter;
     }
 
+    /**
+     * Apply specified SDK constraint.
+     *
+     * @param sdk Indicate which SDK version will be applied.
+     *
+     * @return Another {@link PubspecEnvironment} which contains preferred SDK constraint.
+     *
+     * @throws IllegalVersionConstraintException Applying <code>sdk</code> with {@link PubConstraintPattern#CARET} or
+     *                                           missing {@link PubSemVerConstraint#start() lower constraint}.
+     */
     @Nonnull
     public PubspecEnvironment changeSDK(@Nonnull PubSemVerConstraint sdk) throws IllegalVersionConstraintException {
         return new PubspecEnvironment(sdk, this.flutter);
     }
 
+    /**
+     * Apply specified Flutter constraint.
+     *
+     * @param flutter Indicate which Flutter version will be applied, <code>null</code> when won't prefer.
+     *
+     * @return Another {@link PubspecEnvironment} which contains preferred Flutter constraint.
+     */
     @Nonnull
     public PubspecEnvironment changeFlutter(@Nullable PubSemVerConstraint flutter) {
         try {
