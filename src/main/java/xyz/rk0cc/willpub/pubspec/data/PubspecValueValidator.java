d910d6bf1ada90bcb1717d83907cb286f1c5e9ca
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-15 12:40:48 +0800

    Doc validator

--- src/main/java/xyz/rk0cc/willpub/pubspec/data/PubspecValueValidator.java
+++ src/main/java/xyz/rk0cc/willpub/pubspec/data/PubspecValueValidator.java
@@ -7,14 +7,27 @@ import java.net.URL;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
+/**
+ * Validating incoming apply data of {@link Pubspec} are following the rules.
+ *
+ * @since 1.0.0
+ */
 public final class PubspecValueValidator {
     /**
-     * You can not create {@link PubspecValueValidator} and always throw {@link UnsupportedOperationException}.
+     * You can not create {@link PubspecValueValidator}.
      */
-    private PubspecValueValidator() {
-        throw new UnsupportedOperationException("Validator contains static method only");
-    }
+    private PubspecValueValidator() {}
 
+    /**
+     * Check the package naming is not reserve keyword using lowercase with number and underscore only.
+     *
+     * @param packageName Package name that coming to apply.
+     *
+     * @return <code>true</code> if is follow.
+     *
+     * @see <a href="https://dart.dev/tools/pub/pubspec#name">Pubspec naming</a>
+     * @see <a href="https://dart.dev/guides/language/language-tour#keywords">Dart's keywords table</a>
+     */
     public static boolean packageNaming(@Nonnull String packageName) {
         return Pattern.matches("^[a-z0-9_]+$", packageName)
                 && Stream.of( // Reserve keyword from https://dart.dev/guides/language/language-tour#keywords
@@ -55,19 +68,49 @@ public final class PubspecValueValidator {
                 .noneMatch(packageName::equals);
     }
 
+    /**
+     * Check the giving {@link URL#getProtocol()} is <code>HTTP</code> or <code>HTTPS</code>.
+     *
+     * @param url An {@link URL} for validation.
+     *
+     * @return <code>true</code> is using <code>HTTP</code> or <code>HTTPS</code>.
+     */
     public static boolean httpProtocolOnly(@Nonnull URL url) {
         return Pattern.matches("^https?$", url.getProtocol());
     }
 
+    /**
+     * Assertion of {@link PubspecValueValidator}.
+     * <br/>
+     * All method does not return anything but throw {@link Exception} if returned <code>false</code> from the
+     * validator.
+     *
+     * @since 1.0.0
+     */
     public static final class ValueAssertion {
-        private ValueAssertion() {
-            throw new UnsupportedOperationException("No constructor for assertion");
-        }
+        private ValueAssertion() {}
 
+        /**
+         * Asserting package naming.
+         *
+         * @param packageName Package naming.
+         *
+         * @throws IllegalPubPackageNamingException When {@link PubspecValueValidator#packageNaming(String)} return
+         *                                          <code>false</code>.
+         */
         public static void assertPackageNaming(@Nonnull String packageName) throws IllegalPubPackageNamingException {
             if (!packageNaming(packageName)) throw new IllegalPubPackageNamingException(packageName);
         }
 
+        /**
+         * Asserting package naming with custom message if throwing {@link IllegalPubPackageNamingException}.
+         *
+         * @param packageName Package naming.
+         * @param assertFailedMessage Message shown if assert failed.
+         *
+         * @throws IllegalPubPackageNamingException When {@link PubspecValueValidator#packageNaming(String)} return
+         *                                          <code>false</code>.
+         */
         public static void assertPackageNaming(
                 @Nonnull String packageName,
                 @Nonnull String assertFailedMessage
