5b10e0df970fd9cd806174f727537414b165b3a4
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-15 12:29:34 +0800

    Doc pubspec struct

--- src/main/java/xyz/rk0cc/willpub/pubspec/data/PubspecStructure.java
+++ src/main/java/xyz/rk0cc/willpub/pubspec/data/PubspecStructure.java
@@ -8,43 +8,113 @@ import javax.annotation.Nullable;
 import java.net.URL;
 import java.util.Map;
 
+/**
+ * Interface that defining <code>pubspec.yaml</code> field structure to {@link Pubspec} and {@link PubspecSnapshot}.
+ *
+ * @since 1.0.0
+ */
 interface PubspecStructure {
+    /**
+     * <code>name</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package name.
+     */
     @Nonnull
     String name();
 
+    /**
+     * <code>environment</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package environment.
+     */
     @Nonnull
     PubspecEnvironment environment();
 
+    /**
+     * <code>version</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package version.
+     */
     @Nullable
     SemVer version();
 
+    /**
+     * <code>description</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package description.
+     */
     @Nullable
     String description();
 
+    /**
+     * <code>publish_to</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package publish location.
+     */
     @Nullable
     String publishTo();
 
+    /**
+     * <code>homepage</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package homepage.
+     */
     @Nullable
     URL homepage();
 
+    /**
+     * <code>repository</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package repository.
+     */
     @Nullable
     URL repository();
 
+    /**
+     * <code>issue_tracker</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package issue tracker.
+     */
     @Nullable
     URL issueTracker();
 
+    /**
+     * <code>documentation</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package documentation.
+     */
     @Nullable
     URL documentation();
 
+    /**
+     * <code>dependencies</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package dependencies.
+     */
     @Nonnull
     ImportedReferenceSet dependencies();
 
+    /**
+     * <code>dev_dependencies</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package development dependencies.
+     */
     @Nonnull
     ImportedReferenceSet devDependencies();
 
+    /**
+     * <code>dependency_overrides</code> in <code>pubspec.yaml</code>.
+     *
+     * @return Package override dependencies.
+     */
     @Nonnull
     OverrideReferenceSet dependencyOverrides();
 
+    /**
+     * Fields in <code>pubspec.yaml</code> which does not declare in this interface.
+     *
+     * @return Additional field map data.
+     */
     @Nonnull
     Map<String, Object> additionalData();
 }
